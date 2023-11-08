6ba75a562960dc2772482754db1b6fd62577b766
HugoWinterGhost <hfief1806@gmail.com>
2023-11-08 19:29:16 +0100

    fix connectionstring

--- FrontEndProject/src/app/core/api/base-api.service.ts
+++ FrontEndProject/src/app/core/api/base-api.service.ts
@@ -3,5 +3,5 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class BaseApiUrl {
-  public static readonly apiUrl: string = 'http://localhost:62785/api/student';
+  public static readonly apiUrl: string = 'http://localhost:62785/api/User';
 }
